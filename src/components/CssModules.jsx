d89bbb4e3aa8223ffa614d63df61973694d34d8d
hikida <ponponda0103@gmail.com>
2024-06-16 20:43:12 +0900

    git改行設定&CssModules install

---
+++ src/components/CssModules.jsx
@@ -0,0 +1,9 @@
+export const CssModules= () =>{
+    return(
+        <div>
+            <p>CssModulesです</p>
+            <button>ボタン</button>
+        </div>
+    );
+    //return <p style={contentStyle}>{children}</p>
+};
