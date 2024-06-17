8294f3b173bdc04509e3c5c00fd38ad338924968
Yuu Hikida <ponponda0103@gmail.com>
2024-06-17 20:59:04 +0900

    公式チュートリアル実践

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
+import React from "react";
 import ReactDom from "react-dom";
-import {App} from "./App";
+import App from "./App";
 
-// const App =()=>{
-//     // return null;
-//     return (
-//     <>
-//     <h1>こんちはa"</h1>
-//     <p>aお元気ですか？</p>   
-//     </>
-//     );
-// };
 
 ReactDom.render(<App />,document.getElementById("root"));
